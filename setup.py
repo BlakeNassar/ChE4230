0f478974a7db07bdea03ce0362e216df0f83d440
Blake Nassar <bnassa1@lsu.edu>
2023-03-25 10:14:24 -0500

    Fixed my shit

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 requirements = [
- "scikit-learn==1.0.2",
- "scipy==1.8.0",
- "numpy==1.21.5"
- ]
+    "scikit-learn==1.0.2",
+    "scipy==1.8.0",
+    "numpy==1.21.5"
+]
 setup(
-    name="NameOfThePrimaryFolder",
-    version="0.0.1",
-    author="Your Name",
-    author_email="youremail@lsu.edu",
-    description="AI Super Awesome Tool",
+    name="ChE4230",
+    version="1.0.0",
+    author="Blake Nassar",
+    author_email="bnassa1@lsu.edu",
+    description="I be coding and shit",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/lbrice1/meters",
+    url="https://github.com/BlakeNassar/ChE4230",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
